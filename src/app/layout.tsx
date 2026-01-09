f26d19fb3e28ecbae774d304b3bb9f3aaa731861
devOps-pri <princevirani8822@gmail.com>
2026-01-09 14:18:36 +0530

    adx load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,24 +78,22 @@ export default function RootLayout({
 				/>
 				{/* Google AdX */}
 				<script 
-						async 
-						src="https://securepubads.g.doubleclick.net/tag/js/gpt.js" 
-						crossOrigin="anonymous"
-						></script>
-						
-						{/* Initialize the command queue immediately */}
-						<script
-						dangerouslySetInnerHTML={{
-							__html: `window.googletag = window.googletag || {cmd: []};`,
-						}}/>
+					async 
+					src="https://securepubads.g.doubleclick.net/tag/js/gpt.js" 
+					crossOrigin="anonymous"
+				></script>
+				<script
+					dangerouslySetInnerHTML={{
+					__html: `window.googletag = window.googletag || {cmd: []};`,
+					}}
+				/>
 
 				{/* Google AdSense */}
-				<script
+				{/* <script
 					async
 					crossOrigin='anonymous'
 					src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4752801785953456'
-				/>
-
+				/> */}
 				<meta name='application-name' content='coinmoro Quiz' />
 				<meta name='format-detection' content='telephone=no' />
 				<meta name='theme-color' content='#000000' />
